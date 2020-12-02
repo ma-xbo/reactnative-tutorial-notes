71a671991c2e93691a79b9f6a586efb54d915f4f
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-02 15:23:45 +0100

    HomeScreen um StackNavigator ergänzt

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack'
 
-export default function Home() {
+
+export default function HomeStack() {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='Home' component={Home} />
+            <Stack.Screen name='Details' component={Details} />
+            <Stack.Screen name='Edit' component={Edit} />
+        </Stack.Navigator>
+    )
+}
+
+function Home(props) {
+    const navigation = props.navigation;
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+            <Text>Meine Notizen</Text>
+            <Button title='View' onPress={() => navigation.navigate('Details')} />
+        </View>
+    );
+}
+
+function Details(props) {
+    const navigation = props.navigation;
+    return (
+        <View style={styles.container}>
+            <Text>Details</Text>
+            <Button title='Edit' onPress={() => navigation.navigate('Edit')} />
+        </View>
+    );
+}
+
+function Edit() {
+    return (
+        <View style={styles.container}>
+            <Text>Edit</Text>
         </View>
     );
 }
