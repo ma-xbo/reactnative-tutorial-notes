f8829cecfbc31373e9bec08ec420c4bdfab90871
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-02 15:08:17 +0100

    Icons und Farben der BottomNavigation ändern

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer, TabActions } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './screens/Home';
@@ -9,7 +10,30 @@ export default function App() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            switch (route.name) {
+              case 'Home':
+                iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+                break;
+              case 'Settings':
+                iconName = focused ? 'ios-list-box' : 'ios-list';
+                break;
+              default:
+                break;
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#6097f0',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name='Home' component={Home} />
         <Tab.Screen name='Settings' component={Settings} />
       </Tab.Navigator>
